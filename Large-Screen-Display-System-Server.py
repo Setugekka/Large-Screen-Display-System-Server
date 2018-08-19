39655f38d8781c9c907784832d3f7b151bfd5614
dnn1123 <603261164@qq.com>
2018-08-19 12:37:39 +0800

    nothing

--- Large-Screen-Display-System-Server.py
+++ Large-Screen-Display-System-Server.py
@@ -8,7 +8,6 @@ app = create_app('webapp.config.%sConfig' % env.capitalize())
 manager = Manager(app)
 manager.add_command("server", Server())
 
-
 @manager.shell
 def make_shell_context():
     return dict(app=app,db=db)
