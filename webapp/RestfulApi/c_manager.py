3beffd9e77355c3383ef35b202f286041ca8c77b
Setugekka <fzjie1006@sina.com>
2018-10-18 15:07:46 +0800

    1018

--- webapp/RestfulApi/c_manager.py
+++ webapp/RestfulApi/c_manager.py
@@ -12,7 +12,8 @@ c_manager_blueprint = Blueprint(
     __name__,
     url_prefix="/c_manager"
 )
-
+def dataFormatter(rec):
+    return {'Id':rec.Id,'Name':rec.Name,'Post':rec.Post,'Birthday':rec.Birthday,'Education':rec.Education,'Department':rec.Department,'Major':rec.Major,'Phone':rec.Phone,'Date_training':rec.Date_training,'City':rec.City}
 @c_manager_blueprint.route('/get_by_city',methods=('GET', 'POST'))
 def num_by_city():
     data=db.session.query(p_city_manager.City,db.func.count('*').label("dcount")).group_by(p_city_manager.City).all()
@@ -41,4 +42,12 @@ def num_by_education_city():
         value_list.append(i[1])
     return response(jsonify({"property": "p_manager_distribution_by_major_city", "city": city, "education_list": education_list,"value_list":value_list}))
 
-
+@c_manager_blueprint.route('/get_data_by_city/<city>',methods=('GET', 'POST'))
+def get_data_by_city(city):
+    if city == '':
+        city = "all"
+        data = p_city_manager.query.all()
+    else:
+        data = p_city_manager.query.filter(p_city_manager.City == city).all()
+    result_list = map(dataFormatter,data)
+    return response(jsonify({'data':result_list}))
